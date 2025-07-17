bddfe7ad7f01e5f2ec21b94585710fb32648de8f
翟持江 <129171955+2513502304@users.noreply.github.com>
2025-07-17 12:52:37 +0800

    Update main.py，添加了raw_artworks_by_daily方法用于获取按照时间段筛选的数据

--- main.py
+++ main.py
@@ -13,16 +13,30 @@ async def main() -> None:
         crawl_type=settings.crawl_type,
         sort_type=settings.sort_type,
     )
-    # 获取作品集元数据
-    raw_data = await spider.raw_artworks(
-        query=settings.query,
-        delay=settings.delay,
-    )
-    logger.info(f'number of {len(raw_data) = }')
+    try:
+        # # 获取作品集元数据
+        # raw_data = await spider.raw_artworks(
+        #     query=settings.query,
+        #     delay=settings.delay,
+        # )
+        # logger.info(f'number of {len(raw_data) = }')
+
+        # 获取作品集元数据，并通过时间段筛选
+        raw_data_by_daily = await spider.raw_artworks_by_daily(
+            query=settings.query,
+            delay=settings.delay,
+            start_day=settings.start_day,
+            end_day=settings.end_day,
+        )
+        logger.info(f'number of {len(raw_data_by_daily) = }')
+
+    except NotImplementedError as e:
+        logger.error(f'NotImplementedError: {e}')
+        return
 
     # 简单地存储为 json 文件
     async with aiofiles.open(settings.output_file, mode='wb') as f:
-        await f.write(orjson.dumps(raw_data))
+        await f.write(orjson.dumps(raw_data_by_daily))
 
 
 if __name__ == "__main__":
