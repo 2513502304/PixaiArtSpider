30ad4a378d17c40f9d3018a00d6aa72ba9017ce9
翟持江 <129171955+2513502304@users.noreply.github.com>
2025-07-19 16:00:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,42 +1,26 @@
-from factory import create_crawl_factory, CrawlType, SortType
-from utils import logger
-import settings
+import asyncio
 
 import aiofiles
-import asyncio
 import orjson
+import settings
+from utils import logger
+from core import Platform, PixaiPlatform
 
 
 async def main() -> None:
-    # 创建爬虫
-    spider = create_crawl_factory(
+    # 创建平台爬虫
+    platform = PixaiPlatform(
         crawl_type=settings.crawl_type,
         sort_type=settings.sort_type,
     )
-    try:
-        # # 获取作品集元数据
-        # raw_data = await spider.raw_artworks(
-        #     query=settings.query,
-        #     delay=settings.delay,
-        # )
-        # logger.info(f'number of {len(raw_data) = }')
+    
+    # 启动爬虫
+    raw_artworks = await platform.start()
 
-        # 获取作品集元数据，并通过时间段筛选
-        raw_data_by_daily = await spider.raw_artworks_by_daily(
-            query=settings.query,
-            delay=settings.delay,
-            start_day=settings.start_day,
-            end_day=settings.end_day,
-        )
-        logger.info(f'number of {len(raw_data_by_daily) = }')
-
-    except NotImplementedError as e:
-        logger.error(f'NotImplementedError: {e}')
-        return
-
+    # TODO: 解析原始数据并将其适配多种存储类型
     # 简单地存储为 json 文件
     async with aiofiles.open(settings.output_file, mode='wb') as f:
-        await f.write(orjson.dumps(raw_data_by_daily))
+        await f.write(orjson.dumps(raw_artworks))
 
 
 if __name__ == "__main__":
